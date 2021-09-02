cfa8def7d8179e7a497a8a0d616a9477d3e8a653
wujiali <969606738@qq.com>
2021-09-02 23:15:51 +0800

    first

---
+++ travel/src/main/java/com/wjl/travel/TravelApplication.java
@@ -0,0 +1,14 @@
+package com.wjl.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelApplication.class, args);
+        System.out.println("==========启动成功==========");
+    }
+
+}
